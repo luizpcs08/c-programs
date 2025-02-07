81913c7aac3f4c25d442adc781c0f51c2bf3a697
luizpcs008 <luizpsilva.2005@gmail.com>
2025-02-07 12:56:37 -0300

    Improved code appearence; deletes function selector; modify program to only solve n queen problem

--- chess/chess.c
+++ chess/chess.c
@@ -1,26 +1,26 @@
-/*This program ...
+/*my first backtracking program :D
 
-For implementing more pacific pieces challenges, need to add
-the piece type in check diagonal/horizontal/vertical functions 
-conditions*/
+  The initial idea of this program was to code some chess problems
+  solutions, in a way that user could select which problem would
+  be solved.
+  
+  But at the end, I've just code the N_Queen problem. Because of 
+  it, I've made the stuct of the code in a way that inserting
+  new piece types or new problems would be easier*/
 
 #include <stdio.h>
 #include <string.h>
 
 #define I_BOARD 8
 #define J_BOARD 8
-#define P_NAME_MAX 64
 
 //define the type P(piece)
 typedef struct{
     int type, x, y;
 }P;
 
-
-
-P pieces[(I_BOARD * J_BOARD)];
-
-int n_pieces = 0;
+P pieces[I_BOARD];
+int n_pieces;
 
 /*Returns a piece pointer by giving its x and y position on the
   board*/
@@ -31,17 +31,20 @@ P* get_p(int x, int y){
 
         //gets the piece that has the same x and y coordinates
         if( (pieces[i].x == x) && (pieces[i].y == y) ){
-            
             return &pieces[i];
         }
     }
+
+    //If the piece couldnt be found (some error happened)
+    printf("ERROR: piece couldnt be find in get_p function\n");
+    return &(P) {.type = -1};
 }
 
-//Returns 1 if there is a piece on that coordinate; 0 if it doesnt
+//Returns 1 if there is a piece on that coordinate; 0 if there isnt
 int check_coordinates(int x, int y){
     //loops pieces vector
     for(int i = 0; i < n_pieces; i++){
-        //gets the piece that has the same x and y coordinates
+
         if( (pieces[i].x == x) && (pieces[i].y == y) ){
              return 1;
         }
@@ -57,15 +60,7 @@ void print_board(){
         for(int y = 0; y < J_BOARD; y++){
 
             if(check_coordinates(x, y)){
-                P* p = get_p(x, y);
-                switch(p -> type){
-                    case 1: 
-                        printf("Q ");
-                    break;
-
-                    default:
-                        printf("! ");
-                }
+                printf("Q ");
             }
             else{
                 printf("# ");
@@ -75,11 +70,6 @@ void print_board(){
         printf("\n");
     }
     printf("\n\n");
-}
-
-void end_message(){
-    printf("Obrigado por usar! :D\n");
-    return;
 }
 
 //Gets the piece name stores it in p_name parameter.
@@ -94,31 +84,11 @@ void get_p_name(int aux, char p_name[]){
     }
 }
 
-/*Returns the attack type of a piece by receiving a piece.
-  Can have 3 outputs
-  1: diagonal attack only (bishop)
-  2: vertical and horizontal attack (rook)
-  3: type 1 and 2 (queen)*/
-int attack_type(P p){
-    switch (p.type){
-
-    case 1:
-        return 3;
-    break;
-    
-    default:
-        printf("Error: couldnt identify piece attack type\n");
-    break;
-    }
-
-    return 0;
-}
-
 /*Returns 1 if piece 1 can attack piece 2 in its horizontal*/
 int horizontal_attack(P p1, P p2){
 
-    //checks if they are in the same x  and if p1 can attack horizontally
-    if((p1.x == p2.x) && (attack_type(p1) > 1)){
+    //checks if they are in the same x
+    if(p1.x == p2.x){
 
         //guarantees that p1 is to the left of p2
         if(p1.y > p2.y){
@@ -147,9 +117,8 @@ int horizontal_attack(P p1, P p2){
 /*Returns 1 if piece 1 can attack piece 2 in its vertical*/
 int vertical_attack(P p1, P p2){
 
-    //checks if they are in the same y coordinate and if p1 
-    //can attack vertically
-    if((p1.y == p2.y) && (attack_type(p1) > 1)){
+    //checks if they are in the same y coordinate 
+    if(p1.y == p2.y){
 
         //guarantees that p1 is above p2
         if(p1.x > p2.x){
@@ -191,7 +160,7 @@ int diagonal_attack(P p1, P p2){
 
     //checks if p1 and p2 are in the same diagonal and if p1 
     //can attack diagonally
-    if( (slope == 1 || slope == -1) && (attack_type(p1) == 3 || attack_type(p1) == 1) ){
+    if(slope == 1 || slope == -1){
         
         //guarantees that p1 is to the left of p2
         if(p1.y > p2.y){
@@ -239,11 +208,7 @@ void delete_p(P* p){
     return;
 }
 
-/*Receives coordinates of a square and piece type. Returns
-  if the square is pacific to the piece that is there. 
-  Outputs:
-  0 - Piece can attack or is being attacked
-  1 - piece is on a pacific square*/
+/*Returns 1 if a piece on a square cannot attack or be attacked by any other piece*/
 int check_pacific_square(P* p){
 
     //loops all pieces
@@ -259,48 +224,52 @@ int check_pacific_square(P* p){
             
         }
     }
-    
-    //if the program didnt find any piece that satisfy condition
-    //related to output 0, returns 1
-    return 1;
 
+    return 1;
 }
 
-/*Recursive function
-  Insert 8 queens on the board in a way that they dont attack
-  themselves
-  x parameter stores the row that a queen will be inserted,
-  moved or deleted*/
-void insert_pacific_q(int x, int cont){
-    cont ++;
+//fill all pieces vector with type 0 pieces (empty)
+void place_pieces(){
+    n_pieces = 0;
+    for(int i = 0; i < I_BOARD * J_BOARD; i++){
+        P p = {.type = 0, .y = -1};
+        pieces[i] = p;
+    }
+}
+
+/*Recursive function that solves N queen problem*/
+void problem_solver(int x){
 
     //base case
     if(x == I_BOARD){
-        printf("A solution was found\n");
+        printf("A solution was found!\n\n");
         return;
     }
     else if(x < 0){
-        printf("ERROR: a solution couldnt be founded");
+        printf("ERROR: a new solution couldnt be founded.\nRestarting the board...\n\n");
+        place_pieces();
+        problem_solver(0);
         return;
     }
    
     //Gets piece of the row or creates it if it doesnt exists
     P* p = &pieces[x];
+    p -> x = x;
 
     if(p -> type == 0){
-        p -> x = x;
         p -> y = -1;
         p -> type = 1;
     }
+    
 
-    //loops squares  p
+    //loops squares on the x row
     for(int y = (p -> y) + 1; y <= J_BOARD; y++){
 
         //if overflow, go to previous line
         if(y == J_BOARD){
             delete_p(p);
             n_pieces--;
-            insert_pacific_q(x - 1, cont);
+            problem_solver(x - 1);
             break;
         }
 
@@ -309,105 +278,30 @@ void insert_pacific_q(int x, int cont){
         //try to moves queen to next possible square on the row
         if(check_pacific_square(p)){
             n_pieces++;
-            insert_pacific_q(x + 1, cont);
+            problem_solver(x + 1);
             break;
         }
     }
 }
 
-void tabuleiro_da_paz(){
-    printf("Selecione o tipo da peça:\n 1: Dama\n");
-
-    //stores users input and piece type that will be used
-    int aux;
-    scanf("%d", &aux);
-
-    //reads user input
-    switch(aux){
-        case -1: 
-            function_selector();
-            return;
-        break;
-
-        case 0:
-        end_message();
-        return;
-        break;
-
-        case 1:
-            printf("Dama?... interessante huahauhauaha\n");
-        break;
-
-        default:
-            printf("Tá difícil... vou te dar outra chance!\n");
-            tabuleiro_da_paz();
-            return;
-    }
-
-    //gets the P name by using the aux variable
-    char p_name[P_NAME_MAX];
-    get_p_name(aux, p_name);
-    printf("Inserindo o máximo de %s possível \n", p_name);
-
-    //call insert function and print board
-    insert_pacific_q(0, 0);
-    print_board();
-
-    printf("Selecione a próxima ação:\n1: Voltar ao seletor de funções\n0: Encerrar programa\n");
-    scanf("%d", &aux);
-
-    switch(aux){
-        case 1:
-            function_selector();
-            return;
-        break;
-
-        case 0:
-            end_message();
-            return;
-        break;
-
-        default:
-            return;
-    }
-
-
+void n_queen_problem(int x){
+    printf("Solving N queen problem...\n");
+    problem_solver(x);
 }
 
-void function_selector(){
-
-    printf("Selecione uma função pra chamar purfavô:\n (Digite 0 para encerrar o programa)\n 1: tabuleiro da paz\n");
-
-    int aux;
-    scanf("%d", &aux);
-
-    //reads user input
-    switch(aux){
-        case 0:
-            end_message();
-            return;
-        break;
-
-        case 1:
-            printf("Esse programa adiciona algumas peças no tabuleiro de xadrez, e pede para que você adicione mais peças de forma que elas não se ataquem.\n (Digite 0 para encerrar o programa e -1 para voltar a seleção de funções)\n");
-            tabuleiro_da_paz();
-        break;
-
-        default:
-            printf("Você não sabe ler? Digita um numéro válido!!\n\n");
-            function_selector();
-    }
-}   
-
 int main(){
-
-    //fill all pieces vector with type 0 pieces (empty)
-    for(int i = 0; i < I_BOARD * J_BOARD; i++){
-        P p = {.type = 0};
-        pieces[i] = p;
+    place_pieces();
+    
+    int check = 0;
+    do{
+        printf("Solving N_Queen problem...\n");
+        (check == 0) ? problem_solver(0) : problem_solver(I_BOARD - 1);
+        print_board();
+        printf("Do you want another solution? (Type 1 or 0))\n");
+        scanf("%d", &check);
     }
-    
-    function_selector();
-    
+    while (check);
+
+    printf("Thank you for using my code! ;D\n");
     return 0;
 }
